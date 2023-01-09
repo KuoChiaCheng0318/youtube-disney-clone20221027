8b391ef73dbd02a6ef9f7d5fcfeb9d357c6619ee
KuoChiaCheng0318 <82024833+KuoChiaCheng0318@users.noreply.github.com>
2023-01-08 23:38:20 -0700

    Update Header.js
    Add sign in function with Redux

--- src/components/Header.js
+++ src/components/Header.js
@@ -29,44 +29,49 @@ const Header = (props) =>{
         );
       };
 
-    return <Nav>
+    return (<Nav>
         <Logo>
             <img src="/images/logo.svg" alt="Disney+" />
         </Logo>
 
-        {!userName ? 
-            <Login onClick={handleAuth}>Login</Login> :<></>
-        }    
+        {!userName ? (
+            <Login onClick={handleAuth}>Login</Login>
+            ) : (
+                <>
+                    <NavMenu>
+                        <a href='/home'>
+                            <img src="/images/home-icon.svg" alt="HOME" />
+                            <span>HOME</span>
+                        </a>
+                        <a>
+                        <img src="/images/search-icon.svg" alt="SEARCH" />
+                        <span>SEARCH</span>
+                        </a>
+                        <a>
+                        <img src="/images/watchlist-icon.svg" alt="WATCHLIST" />
+                        <span>WATCHLIST</span>
+                        </a>
+                        <a>
+                        <img src="/images/original-icon.svg" alt="ORIGINALS" />
+                        <span>ORIGINALS</span>
+                        </a>
+                        <a>
+                        <img src="/images/movie-icon.svg" alt="MOVIES" />
+                        <span>MOVIES</span>
+                        </a>
+                        <a>
+                        <img src="/images/series-icon.svg" alt="SERIES" />
+                        <span>SERIES</span>
+                        </a>
+                    </NavMenu>
+                    <UserImg src={userPhoto} alt={userName} />
 
-        <NavMenu>
-            <a href='/home'>
-                <img src="/images/home-icon.svg" alt="HOME" />
-                <span>HOME</span>
-            </a>
-            <a>
-              <img src="/images/search-icon.svg" alt="SEARCH" />
-              <span>SEARCH</span>
-            </a>
-            <a>
-              <img src="/images/watchlist-icon.svg" alt="WATCHLIST" />
-              <span>WATCHLIST</span>
-            </a>
-            <a>
-              <img src="/images/original-icon.svg" alt="ORIGINALS" />
-              <span>ORIGINALS</span>
-            </a>
-            <a>
-              <img src="/images/movie-icon.svg" alt="MOVIES" />
-              <span>MOVIES</span>
-            </a>
-            <a>
-              <img src="/images/series-icon.svg" alt="SERIES" />
-              <span>SERIES</span>
-            </a>
-        </NavMenu>
-    <Login onClick={handleAuth}>login</Login>
+                </>
+                )}
+        {/* <Login onClick={handleAuth}>login</Login> */}
     </Nav>
-}
+    );
+};
 
 const Nav = styled.nav`
     position: fixed;
@@ -179,6 +184,10 @@ const Login =styled.a`
     }
 
 `
+
+const UserImg = styled.img`
+  height: 100%;
+`;
     
 
 
